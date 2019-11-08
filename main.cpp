6f390c54432b68707c1095fae27ee5a09f0967da
Yassine Goeller <yassine.goeller@uha.fr>
2019-11-08 13:42:05 +0100

    Initialisation du projet

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  robot_ecraseur
+//
+//  Created by Yassine Goeller on 08/11/2019.
+//  Copyright © 2019 Yassine Goeller. All rights reserved.
+//
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout<<"Hello World !!"<<std::endl;
+        return 0;
+}
