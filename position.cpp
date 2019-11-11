2b7c8cd85bbd8dc3d75f51bf7b4557d1802513c3
Yassine Goeller <yassine.goeller@uha.fr>
2019-11-11 17:20:08 +0100

    creation de position.cpp et position.hpp creation de l'entete de la classe position

---
+++ position.cpp
@@ -0,0 +1,10 @@
+//
+//  position.cpp
+//  robot_ecraseur
+//
+//  Created by Yassine Goeller on 11/11/2019.
+//  Copyright © 2019 Yassine Goeller. All rights reserved.
+//
+
+#include "position.hpp"
+
---
+++ position.hpp
@@ -0,0 +1,16 @@
+//
+//  position.hpp
+//  robot_ecraseur
+//
+//  Created by Yassine Goeller on 11/11/2019.
+//  Copyright © 2019 Yassine Goeller. All rights reserved.
+//
+
+#ifndef position_hpp
+#define position_hpp
+
+#include <stdio.h>
+class position{
+    
+};
+#endif /* position_hpp */
