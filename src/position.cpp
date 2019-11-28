72df12d8e81ce852349c180aed05ea2bcfafad6c
yassinegoeller <yassine.goeller1@outlook.fr>
2019-11-28 15:32:53 +0100

    Modification de position.cpp Implementation du constructeur par defaut

--- src/position.cpp
+++ src/position.cpp
@@ -9,18 +9,3 @@
 #include "position.hpp"
 
 position::position() : d_x{0}, d_y{0}{}
-position::position(int x, int y) : d_x{x}, d_y{y}{}
-int position::getPosX() const
-{return d_x;}
-int position::getPosY() const
-{ return d_y;}
-void position::seDeplaceEn(int x, int y)
-{
-    d_x = x;
-    d_y = y;
-}
-void position::seDeplaceDe(int x, int y)
-{
-    d_x += x;
-    d_y += y;
-}
