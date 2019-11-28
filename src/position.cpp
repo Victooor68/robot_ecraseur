bdd9527b06348eb0dec17df57cff79aa5a681428
yassinegoeller <yassine.goeller1@outlook.fr>
2019-11-28 15:45:14 +0100

    Modification de position.cpp Implementation de getPosY

--- src/position.cpp
+++ src/position.cpp
@@ -12,3 +12,5 @@ position::position() : d_x{0}, d_y{0}{}
 position::position(int x, int y) : d_x{x}, d_y{y}{}
 int position::getPosX() const
 {return d_x;}
+int position::getPosY() const
+{return d_y;}
