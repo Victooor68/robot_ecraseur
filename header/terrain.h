12d2c730ff36c813bea7cd1b1425eb06b4a9c7e6
Erdneael <57407691+Erdneael@users.noreply.github.com>
2019-12-13 15:01:18 +0100

    Correction

--- header/terrain.h
+++ header/terrain.h
@@ -10,18 +10,24 @@ class terrain
     public:
         terrain(int largeur, int hauteur);
         terrain( std::vector<std::vector<int>> terrain);
-        virtual ~terrain();
+    virtual ~terrain();
 
-        std::vector<std::vector<int>> getTerrain() const;
-        void affiche(std::ostream& os) const;
-        terrain read(std::istream& is);
-        bool estDansTerrain(entite ent, int direction) const;
-        void seDeplaceADroiteSurTerrain(entite ent);
-        void seDeplaceAGaucheSurTerrain(entite ent);
-        void seDeplaceEnHautSurTerrain(entite ent);
-        void seDeplaceEnBasSurTerrain(entite ent);
+    std::vector<std::vector<int>> getTerrain() const;
+    void affiche(std::ostream& os) const;
+    void afficheGraphique() const;
+    int largeur() const;
+    int hauteur() const;
+    terrain read(std::istream& is);
+    bool estDansTerrain(entite ent, int direction) const;
+    void seDeplaceADroiteSurTerrain(entite ent);
+    void seDeplaceAGaucheSurTerrain(entite ent);
+    void seDeplaceEnHautSurTerrain(entite ent);
+    void seDeplaceEnBasSurTerrain(entite ent);
+
+private:
     std::vector<std::vector<int>> d_terrain;
-    private:
+    int d_hauteur;
+    int d_largeur;
 
 };
 
