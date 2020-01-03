3e80d29a72bf68dce8fb9627c429572af6317193
Erdneael <leandre.demuth@hotmail.fr>
2020-01-03 17:30:51 +0100

    Correction EntiteMouvanteTest

--- header/terrain.h
+++ header/terrain.h
@@ -8,14 +8,14 @@
 #include "robot.h"
 #include "joueur.h"
 
-#define BAS_GAUCHE 1
-#define BAS 2
+#define BAS_GAUCHE 9
+#define BAS 6
 #define BAS_DROITE 3
-#define GAUCHE 4
-#define DROITE 6
+#define GAUCHE 8
+#define DROITE 2
 #define HAUT_GAUCHE 7
-#define HAUT 8
-#define HAUT_DROITE 9
+#define HAUT 4
+#define HAUT_DROITE 1
 
 
 class terrain
