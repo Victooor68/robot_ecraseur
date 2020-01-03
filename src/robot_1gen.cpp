eee4890bd1915131a5bf564a64b60c9081b5e659
victorsauter <victor.sauter@uha.fr>
2020-01-03 14:38:39 +0100

    Correction lisibilité robot1gen

--- src/robot_1gen.cpp
+++ src/robot_1gen.cpp
@@ -4,7 +4,6 @@
 
 #include "position.hpp"
 #include <vector>
-#include <iostream>
 #include "../header/robot_1gen.h"
 
 
@@ -18,22 +17,18 @@ void robot_1gen::deplacement_Auto(joueur* j) {
     //index DROITE_DEPLACEMENT
     int score_Deplacement_Droite = abs(d_pos.getPosX() + 1 - j->getPosition().getPosX())
             + abs(d_pos.getPosY() - j->getPosition().getPosY());
-    std::cout<<"score Droite : "<<score_Deplacement_Droite<<std::endl;
 
     //index GAUCHE_DEPLACEMENT
     int score_Deplacement_Gauche = abs(d_pos.getPosX() - 1 - j->getPosition().getPosX())
                                    + abs(d_pos.getPosY() - j->getPosition().getPosY());
-    std::cout<<"score Gauche : "<<score_Deplacement_Gauche<<std::endl;
 
     //index HAUT_DEPLACEMENT
     int score_Deplacement_Haut = abs(d_pos.getPosX() - j->getPosition().getPosX())
                                  + abs(d_pos.getPosY() + 1 - j->getPosition().getPosY());
-    std::cout<<"score haut : "<<score_Deplacement_Haut<<std::endl;
 
     //index BAS_DEPLACEMENT
     int score_Deplacement_Bas = abs(d_pos.getPosX() - j->getPosition().getPosX())
                                + abs(d_pos.getPosY() -1 - j->getPosition().getPosY());
-    std::cout<<"score Bas : "<<score_Deplacement_Bas<<std::endl;
 
     scoreDeplacement.push_back(score_Deplacement_Droite);
     scoreDeplacement.push_back(score_Deplacement_Gauche);
@@ -55,15 +50,12 @@ void robot_1gen::deplacement_Auto(joueur* j) {
     switch(indexMeilleurScore) {
         case DROITE_DEPLACEMENT :
             seDeplaceADroite();
-            std::cout<<"DOITE"<<std::endl;
             break;
         case GAUCHE_DEPLACEMENT :
             seDeplaceAGauche();
-            std::cout<<"GAUHCE"<<std::endl;
             break;
         case HAUT_DEPLACEMENT :
             seDeplaceEnHaut();
-            std::cout<<"ZEBI"<<std::endl;
             break;
         case BAS_DEPLACEMENT :
             seDeplaceEnBas();
